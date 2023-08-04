d145ff040e0b5cb7522f68184f7b4eec90246bbf
ESSO0428 <92996726+ESSO0428@users.noreply.github.com>
2023-08-05 00:48:21 +0800

    Update folds.lua

--- lua/mkdnflow/folds.lua
+++ lua/mkdnflow/folds.lua
@@ -17,77 +17,102 @@
 local M = {}
 
 M.getHeadingLevel = function(line)
-    local level
-    if line then
-        level = line:match('^%s-(#+)')
-    end
-    return (level and string.len(level)) or 99
+  local level
+  if line then
+    level = line:match('^%s-(#+)')
+  end
+  return (level and string.len(level)) or 99
 end
 
 local get_section_range = function(start_row)
-    start_row = start_row or vim.api.nvim_win_get_cursor(0)[1]
-    local line, n_lines = vim.api.nvim_buf_get_lines(0, start_row - 1, start_row, false)[1], vim.api.nvim_buf_line_count(0)
-    local heading_level = M.getHeadingLevel(line)
-    if heading_level > 0 then
-        local continue = true
-        local end_row = start_row + 1
-        while continue do
-            local next_line = vim.api.nvim_buf_get_lines(0, end_row - 1, end_row, false)
-            if next_line[1] then
-                if M.getHeadingLevel(next_line[1]) <= heading_level then
-                    continue = false
-                else
-                    end_row = end_row + 1
-                end
-            elseif end_row <= n_lines then -- Line might just be empty; make sure we're not at end of buffer
-                end_row = end_row + 1
-            else -- End of buffer reached
-                continue = false
-            end
+  start_row = start_row or vim.api.nvim_win_get_cursor(0)[1]
+  local line, n_lines = vim.api.nvim_buf_get_lines(0, start_row - 1, start_row, false)[1], vim.api.nvim_buf_line_count(0)
+  local heading_level = M.getHeadingLevel(line)
+  if heading_level > 0 then
+    local continue = true
+    local end_row = start_row + 1
+    while continue do
+      local next_line = vim.api.nvim_buf_get_lines(0, end_row - 1, end_row, false)
+      if next_line[1] then
+        if M.getHeadingLevel(next_line[1]) <= heading_level then
+          continue = false
+        else
+          end_row = end_row + 1
         end
-        return {start_row, end_row - 1}
+      elseif end_row <= n_lines then -- Line might just be empty; make sure we're not at end of buffer
+        end_row = end_row + 1
+      else                           -- End of buffer reached
+        continue = false
+      end
     end
+    return { start_row, end_row - 1 }
+  end
 end
 
 local get_nearest_heading = function()
-    local row = vim.api.nvim_win_get_cursor(0)[1] - 1
-    local continue = true
-    while continue and row > 0 do
-        local prev_line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
-        if M.getHeadingLevel(prev_line) < 99 then
-            continue = false
-            return row
-        else
-            row = row - 1
-        end
+  local row = vim.api.nvim_win_get_cursor(0)[1] - 1
+  local continue = true
+  while continue and row > 0 do
+    local prev_line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
+    if M.getHeadingLevel(prev_line) < 99 then
+      continue = false
+      return row
+    else
+      row = row - 1
     end
+  end
 end
+
+-- M.foldSection = function()
+--     local line = vim.api.nvim_get_current_line()
+--     if M.getHeadingLevel(line) < 99 then
+--         local range = get_section_range()
+--         if range then
+--             vim.cmd(tostring(range[1])..','..tostring(range[2])..'fold')
+--         end
+--     else
+--         local start_row = get_nearest_heading()
+--         if start_row then
+--             local range = get_section_range(start_row)
+--             if range then
+--                 vim.cmd(tostring(range[1])..','..tostring(range[2])..'fold')
+--             end
+--         end
+--     end
+-- end
 
 M.foldSection = function()
-    local line = vim.api.nvim_get_current_line()
-    if M.getHeadingLevel(line) < 99 then
-        local range = get_section_range()
-        if range then
-            vim.cmd(tostring(range[1])..','..tostring(range[2])..'fold')
-        end
-    else
-        local start_row = get_nearest_heading()
-        if start_row then
-            local range = get_section_range(start_row)
-            if range then
-                vim.cmd(tostring(range[1])..','..tostring(range[2])..'fold')
-            end
-        end
+  local line = vim.fn.line('.')
+  local is_fold_closed = vim.fn.foldclosed(line) ~= -1
+  if is_fold_closed then
+    return vim.cmd([[silent! norm!zo]])
+  end
+
+  local line_content = vim.api.nvim_get_current_line()
+  if M.getHeadingLevel(line_content) < 99 then
+    local range = get_section_range()
+    if range then
+      vim.cmd(tostring(range[1]) .. ',' .. tostring(range[2]) .. 'fold')
     end
+  else
+    local start_row = get_nearest_heading()
+    if start_row then
+      local range = get_section_range(start_row)
+      if range then
+        vim.cmd(tostring(range[1]) .. ',' .. tostring(range[2]) .. 'fold')
+      end
+    end
+  end
 end
 
+
 M.unfoldSection = function(row)
-    row = row or vim.api.nvim_win_get_cursor(0)[1]
-    local foldstart = vim.fn.foldclosed(tostring(row))
-    if foldstart > -1 then
-        local foldend = vim.fn.foldclosedend(tostring(row))
-        vim.cmd(tostring(foldstart)..','..tostring(foldend)..'foldopen')
-    end
+  row = row or vim.api.nvim_win_get_cursor(0)[1]
+  local foldstart = vim.fn.foldclosed(tostring(row))
+  if foldstart > -1 then
+    local foldend = vim.fn.foldclosedend(tostring(row))
+    vim.cmd(tostring(foldstart) .. ',' .. tostring(foldend) .. 'foldopen')
+  end
 end
 
 return M
