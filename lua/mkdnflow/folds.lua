28d9329e60162e88f9c199e1f5fb4630d20a95f6
ESSO0428 <92996726+ESSO0428@users.noreply.github.com>
2023-08-26 22:16:54 +0800

    Update folds.lua create function foldCycle

--- lua/mkdnflow/folds.lua
+++ lua/mkdnflow/folds.lua
@@ -60,7 +60,7 @@ M.getHeadingLevel = function(line, current_line)
   else
     level_v = 99
   end
-  -- print(level_v)
+  -- print(line, level_v)
   return level_v
 end
 
@@ -124,6 +124,7 @@ end
 
 M.foldSection = function()
   local line = vim.fn.line('.')
+  vim.cmd("set foldmethod=manual")
   local is_fold_closed = vim.fn.foldclosed(line) ~= -1
   if is_fold_closed then
     return vim.cmd([[silent! norm!zo]])
@@ -149,10 +150,76 @@ end
 
 M.unfoldSection = function(row)
   row = row or vim.api.nvim_win_get_cursor(0)[1]
+  vim.cmd("set foldmethod=manual")
   local foldstart = vim.fn.foldclosed(tostring(row))
   if foldstart > -1 then
     local foldend = vim.fn.foldclosedend(tostring(row))
     vim.cmd(tostring(foldstart) .. ',' .. tostring(foldend) .. 'foldopen')
+  end
+end
+
+M.global_cycle_mode = 'Overview' -- Initial state
+
+-- Function to get capture using treesitter
+local function getCapture(line_number)
+  return vim.inspect_pos(0, line_number, 0, nil).treesitter[1].capture
+end
+
+-- Check if a line is a markdown header
+local function isMarkdownHeader(line)
+  return string.find(line, '^#+%s') ~= nil
+end
+
+-- Check if a line starts with 4 spaces or a tab
+local function isIndented(line)
+  return line:match("^    ") or line:match("^\t")
+end
+
+-- Check if a line is a header using treesitter
+local function isTreesitterHeader(capture)
+  return capture:match('text.title.(%d).marker') ~= nil
+end
+
+_G.mkdnflowFoldFunction = function(line_number)
+  local success, capture = pcall(getCapture, line_number - 1) -- Adjusting for 0-based index
+  local next_line_success, next_capture = pcall(getCapture, line_number)
+  local line = vim.api.nvim_buf_get_lines(0, line_number - 1, line_number, false)[1]
+  local next_line = vim.api.nvim_buf_get_lines(0, line_number, line_number + 1, false)[1]
+
+  if success and isTreesitterHeader(capture) then
+    return 1 -- Header level
+  elseif not success and isMarkdownHeader(line) then
+    return 1
+  elseif line == '' and (isMarkdownHeader(next_line) or (next_line_success and isTreesitterHeader(next_capture))) then
+    return 0
+  elseif isIndented(line) or (next_line and isIndented(next_line)) then
+    return 2 -- Contents level
+  else
+    return 1
+  end
+
+  return 2 -- Default fold level
+end
+
+M.foldCycle = function()
+  -- Set the fold method to expression and use the foldFunction to determine fold levels
+  vim.cmd("set foldmethod=expr")
+  vim.cmd("set foldexpr=v:lua._G.mkdnflowFoldFunction(v:lnum)")
+
+  -- Now, based on the current cycle mode, perform the folding operation
+  if M.global_cycle_mode == 'Show All' then
+    M.global_cycle_mode = 'Overview'
+    vim.api.nvim_out_write("[mkdnflow] Overview\n")
+    vim.cmd([[silent! norm!zMzX]])
+  elseif M.global_cycle_mode == 'Overview' then
+    M.global_cycle_mode = 'Contents'
+    vim.api.nvim_out_write("[mkdnflow] Contents\n")
+    vim.wo.foldlevel = 1
+    vim.cmd([[silent! norm!zx]])
+  else -- Contents
+    M.global_cycle_mode = 'Show All'
+    vim.api.nvim_out_write("[mkdnflow] Show All\n")
+    vim.cmd([[silent! norm!zR]])
   end
 end
 
